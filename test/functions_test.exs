a34709452a35b2b2ae8219db6b85a81bf5b15dae
Oshan Wisumperuma <warunaoshan@gmail.com>
2021-09-22 12:30:41 +0530

    anonymous functions

--- test/functions_test.exs
+++ test/functions_test.exs
@@ -10,6 +10,16 @@ defmodule Funct do
   def a(_x) do
     :c
   end
+
+  def sum_named(x, y) do
+    sum = fn a, b -> a + b end
+    sum.(x, y)
+  end
+
+  def sum_anon(x, y) do
+    sum = &(&1 + &2)
+    sum.(x, y)
+  end
 end
 
 defmodule FuncTest do
@@ -18,5 +28,13 @@ defmodule FuncTest do
 
   test "functions test" do
     assert Funct.a(12) == :a
+  end
+
+  test "sum named" do
+    assert Funct.sum_named(4, 5) == 9
+  end
+
+  test "sum anonym" do
+    assert Funct.sum_anon(3, 5) == 8
   end
 end
